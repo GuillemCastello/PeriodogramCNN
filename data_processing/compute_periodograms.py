9fb790d4065a4e3a49139a7b5cb81ede35878d4e
guillemc <guillemcastello@gmail.com>
2025-03-03 14:06:04 +0100

    Remove mean before hann to compute periodograms

--- data_processing/compute_periodograms.py
+++ data_processing/compute_periodograms.py
@@ -42,7 +42,7 @@ hann_window = hann_window[:, None, None]
 # Define the x and y ranges
 
 
-#temporal_sequence = time_series #- np.mean(time_series, axis=0)) / np.mean(time_series, axis=0)
+temporal_sequence = time_series - np.mean(time_series, axis=0) #/ np.mean(time_series, axis=0)
 temporal_sequence = time_series * hann_window
 
 time_series = None  # Free memory
